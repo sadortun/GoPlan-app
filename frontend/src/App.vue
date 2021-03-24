6a26ade30054397fa5f049055e19e2c72475d833
Samuel Denis-D'Ortun <sam@sddproductions.com>
2021-03-24 16:31:40 -0400

    Minimum reproductible issue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,46 @@
 <template>
-  <component :is="layout">
-    <router-view v-slot="{ Component }">
+<div>abc
+
 <!--      <suspense>-->
-        <component :is="Component" />
+
+        <trading-vue
+                     :data="chartData" :height="333" :width="444">
+        </trading-vue>
+
 <!--      </suspense>-->
-    </router-view>
-  </component>
+  def
+</div>
 </template>
 
 <script lang="ts">
 import {computed, defineComponent} from 'vue'
 import {useRouter} from 'vue-router'
+import TradingVue from 'trading-vue-js'
 
 const defaultLayout = 'default'
 
 export default defineComponent({
+  components:{
+    TradingVue,
+  },
   setup () {
     const {currentRoute} = useRouter()
-
+    const chartData = ({
+      ohlcv: [
+        [1551128400000, 33, 37.1, 14, 14, 196],
+        [1551132000000, 13.7, 30, 6.6, 30, 206],
+        [1551135600000, 29.9, 33, 21.3, 21.8, 74],
+        [1551139200000, 21.7, 25.9, 18, 24, 140],
+        [1551142800000, 24.1, 24.1, 24, 24.1, 29],
+      ]
+    })
     const layout = computed(
       () => `${currentRoute.value.meta.layout || defaultLayout}-layout`
     )
 
     return {
       layout,
+      chartData,
     }
   },
 })
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,61 +2,8 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
-import DashboardLayout from './layouts/DashboardLayout.vue'
-import EmptyLayout from './layouts/EmptyLayout.vue'
-
-import VueGapi from 'vue-gapi'
-import {AuthStore} from './store'
-
-import './parseConfig'
-import VueApexCharts from 'vue3-apexcharts'
-import * as dayjs from 'dayjs'
-
-
-import * as duration from 'dayjs/plugin/duration'
-
-dayjs.extend(duration)
-
 
 const app = createApp(App)
-
-app.use(VueGapi, {
-  clientId : '625813000498-2etprn7qf2ca8d3hpg0v1if77ihlp231.apps.googleusercontent.com',
-  scope    : 'profile',
-})
-
-app.use(VueApexCharts)
-
-const authStore = new AuthStore()
-
-app.component('DefaultLayout', DashboardLayout)
-app.component('EmptyLayout', EmptyLayout)
-app.provide('$authStore', authStore)
-
-
-// @todo move to router.ts
-router.beforeEach(async (to, _, next) => {
-
-    // const {authStore} = inject('$authStore')
-    if (to.meta && to.meta.requiresAuth === false) {
-        next()
-        return
-    }
-
-    if (await authStore.isAuthenticated() !== true) {
-        next({name: 'auth'})
-        return
-    }
-    // const currentUser = await authStore.currentUser()
-    // if (currentUser.isOnboardingCompleted() && to.name !== 'create-account') {
-    //   next({name: 'profile'})
-    //
-    //   return
-    // }
-
-    next()
-})
-
 
 app.use(router)
 app.mount('#app')
